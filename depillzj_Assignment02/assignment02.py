03ebed387d611f7e6d62b35b1a76a35569f4617d
depillzj <depillzj@LCBLAB3120J-23.uccob.uc.edu>
2023-04-20 11:47:24 -0400

    commit

---
+++ depillzj_Assignment02/assignment02.py
@@ -0,0 +1,24 @@
+#Zavier DePillo
+#depillzj@mail.uc.edu
+#Assignment 02
+#IS 4010
+#Spring 2023
+
+fizz = 99
+#1. fizz is an integer. I know this because it is a whole number.
+print(fizz-1)
+gamma = 'University of Southern Ohio'
+#2. gamma is a string.
+print(gamma + " " + str(fizz))
+delta = 1.5
+#3 delta is a float.
+epsilon = fizz + delta 
+print(epsilon)
+#4. epsilon is a float.
+eta = (gamma[3])
+print('>' + eta + '<')
+nu = 1234.5
+#5. nu is a float.
+print(nu)
+zeta = delta + fizz
+print(zeta)
